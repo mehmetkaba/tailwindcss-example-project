be5393826519f902400b61a3e6ffb17473d2f216
mehmetkaba <gm.mkaba@gmail.com>
2023-02-26 18:03:35 +0300

    colors, fonts, customizing container

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+
+    container: {
+      center: true
+    },
+
+    extend: {
+      fontFamily: {
+        gemunuLibre: ['Gemunu Libre', 'sans-serif'],
+        openSans: ['Open Sans', 'sans-serif'],
+        mynerve: ['Mynerve', 'cursive']
+      },
+      colors: {
+        'fbLaci': '#002d72',
+        'fbSari': '#ffed00'
+      }
+    },
+
   },
   plugins: [],
 }
